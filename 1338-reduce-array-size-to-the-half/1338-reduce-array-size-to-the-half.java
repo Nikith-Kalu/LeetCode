78788b5e397563ffac2b45be48a49fc929ae4523
Nikith Kalu <nikithkalu@gmail.com>
2022-08-18 22:23:44 +1000

    Time: 48 ms (81.03%), Space: 81.3 MB (73.89%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -1,23 +1,29 @@
 class Solution {
     public int minSetSize(int[] arr) {
-        int len = arr.length;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int maxFreq=0;
-        for(int num : arr ){
-            map.put(num, map.getOrDefault(num, 0)+1);
-            maxFreq = Math.max(maxFreq, map.get(num));
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int n = arr.length;
+        n /= 2;
+        for (int i : arr) 
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        
+        if (arr.length == map.size()) 
+            return n;
+        
+        int c = 0, c1 = 0, k = 0;
+        
+        while (c == 0) {
+            //find most frequent character
+            Map.Entry<Integer, Integer> maxEntry = null;
+            for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                if(maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0) 
+                    maxEntry = entry;
+            }
+            c1 += maxEntry.getValue();
+            k++;
+            map.replace(maxEntry.getKey(), Integer.MIN_VALUE);
+            if(c1 >= n) 
+                c = 1;
         }
-        int[] freqArr = new int[maxFreq+1];
-        for(int freq : map.values()){
-            freqArr[freq]++;
-        }
-        int res=0, half=len/2, removed=0, curFreq=maxFreq;
-        while(removed<half){
-            while(freqArr[curFreq]==0) curFreq--;
-            freqArr[curFreq]--;
-            removed+=curFreq;
-            res++;
-        }
-        return res;
-    } 
+        return k;
+    }
 }
