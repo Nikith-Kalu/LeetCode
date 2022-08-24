028bc210672768d5d4bb135e2f38672ce896122c
Nikith Kalu <nikithkalu@gmail.com>
2022-08-24 16:46:01 +1000

    Time: 120 ms (11.82%), Space: 13.6 MB (43.79%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -1,10 +1,19 @@
 class Solution:
     def addTwoNumbers(self, l1, l2):
-        def toint(node):
-            return node.val + 10 * toint(node.next) if node else 0
-        n = toint(l1) + toint(l2)
-        first = last = ListNode(n % 10)
-        while n > 9:
-            n /= 10
-            last.next = last = ListNode(n % 10)
-        return first
+        s1 = ''
+        s2 = ''
+        while l1 != None:
+            s1 += str(l1.val)
+            l1 = l1.next
+        while l2 != None:
+            s2 += str(l2.val)
+            l2 = l2.next
+        
+        sum1 = int(s1[::-1]) + int(s2[::-1])
+        sum1 = str(sum1)
+        head = ListNode(int(sum1[0]))
+        for i in sum1[1:]:
+            node = ListNode(int(i))
+            node.next = head
+            head = node
+        return head
