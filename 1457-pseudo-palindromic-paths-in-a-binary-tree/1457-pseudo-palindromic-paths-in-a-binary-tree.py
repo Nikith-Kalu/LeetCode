850fbc47816051fa4b38e356849bf844c491c675
Nikith Kalu <nikithkalu@gmail.com>
2022-09-14 21:38:52 +1000

    Time: 1679 ms (27.73%), Space: 85.9 MB (43.30%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
@@ -1,25 +1,12 @@
 class Solution:
-    def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
-        ans=[0]
-        def solve(root,path):
+    def pseudoPalindromicPaths (self, root: TreeNode) -> int:
+        def dfs(root, mask):
             if not root:
-                return
-            if root.val in path:
-                path[root.val]+=1
-            else:
-                path[root.val]=1
-            
+                return 0
+            mask = mask ^ (1 << root.val)
             if not root.left and not root.right:
-                o=0
-                for i in path:
-                    if path[i]%2!=0:
-                        o+=1
-                if o in [0,1]:
-                    ans[0]+=1
-                    
-            else:
-                
-                solve(root.left,path.copy())
-                solve(root.right,path.copy())
-        solve(root,{})
-        return ans[0]
+                if mask & (mask - 1) == 0:
+                    return 1
+                return 0
+            return dfs(root.left, mask) + dfs(root.right, mask)
+        return dfs(root, 0)
