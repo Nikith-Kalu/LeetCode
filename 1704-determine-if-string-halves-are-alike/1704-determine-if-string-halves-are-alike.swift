9f53952140f9a79afbc2c7b0e86e3084b3fe858c
Nikith Kalu <nikithkalu@gmail.com>
2024-01-16 16:37:01 +1100

    Time: 7 ms (63.67%), Space: 15.9 MB (23.55%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.swift
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.swift
@@ -1,29 +1,14 @@
 class Solution {
     func halvesAreAlike(_ s: String) -> Bool {
-        let vowels: Set<Character> = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
-        var firstHalfVowelCount = 0, secondHalfVowelCount = 0
+        let vowels = Set("aeiouAEIOU")
         let midIndex = s.index(s.startIndex, offsetBy: s.count / 2)
-        var leftIndex = s.startIndex
-        var rightIndex = midIndex
-
-        while leftIndex < midIndex && rightIndex < s.endIndex {
-            if vowels.contains(s[leftIndex]) {
-                firstHalfVowelCount += 1
-            }
-            if vowels.contains(s[rightIndex]) {
-                secondHalfVowelCount += 1
-            }
-
-            leftIndex = s.index(after: leftIndex)
-            rightIndex = s.index(after: rightIndex)
-
-            // Early exit if difference in counts exceeds remaining characters
-            if abs(firstHalfVowelCount - secondHalfVowelCount) > s.distance(from: rightIndex, to: s.endIndex) {
-                return false
-            }
-        }
-
+        
+        let firstHalf = s[s.startIndex..<midIndex]
+        let secondHalf = s[midIndex..<s.endIndex]
+        
+        let firstHalfVowelCount = firstHalf.filter { vowels.contains($0) }.count
+        let secondHalfVowelCount = secondHalf.filter { vowels.contains($0) }.count
+        
         return firstHalfVowelCount == secondHalfVowelCount
     }
 }
-
