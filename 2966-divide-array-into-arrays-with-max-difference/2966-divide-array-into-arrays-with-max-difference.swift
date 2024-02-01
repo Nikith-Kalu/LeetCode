34db6a283180650796bb9a52233b77d54f95e389
Nikith Kalu <nikithkalu@gmail.com>
2024-02-02 00:04:36 +1100

    Time: 1079 ms (26.79%), Space: 26.7 MB (12.50%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.swift
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.swift
@@ -4,7 +4,6 @@ class Solution {
     var result = [[Int]]()
     
     for i in stride(from: 0, to: sortedNums.count, by: 3) {
-        // Check if the current sub-array of size 3 satisfies the condition
         if sortedNums[i + 2] - sortedNums[i] <= k {
             result.append([sortedNums[i], sortedNums[i + 1], sortedNums[i + 2]])
         } else {
